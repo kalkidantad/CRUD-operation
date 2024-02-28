4efe8f7c4ba275ebf097cf87fc26e6de7b127267
kalkidantad <114928151+kalkidantad@users.noreply.github.com>
2024-02-28 10:36:48 +0300

    Add files via upload

---
+++ employeeProject/register/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from . import views
+
+
+urlpatterns = [
+    path('', views.employee_form, name= 'employee_insert'), #get and post request for insert op
+    path('<int:id>/', views.employee_form, name= 'employee_update'), #get and post request for update op
+    path('delete/<int:id>/', views.employee_delete, name='employee_delete'),
+    path('list/', views.employee_list, name='employee_list')  #get and post request for retrieve op and show all the records
+  
+]
